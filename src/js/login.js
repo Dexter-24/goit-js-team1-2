bd0b387c9765d447fa23ef2a742e8943eda36b75
Denys Bystryi <bystryydenis211999@gmail.com>
2024-07-13 11:49:08 +0300

    second-commit

---
+++ src/js/login.js
@@ -0,0 +1,21 @@
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+import { loginUserService } from './api';
+import { STORAGE_KEY } from './refs';
+
+const instanceLogin = basicLightbox.create(document.querySelector('#login'));
+
+export function handlerLogin() {
+  instanceLogin.show();
+  const loginForm = document.querySelector('.login-form');
+  loginForm.addEventListener('submit', loginUser);
+}
+
+function loginUser(evt) {
+  evt.preventDefault();
+  const { email, password } = evt.currentTarget.elements;
+  loginUserService({
+    email: email.value,
+    password: password.value,
+  }).then(data => localStorage.setItem(STORAGE_KEY, data.token));
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { regBtn, logBtn } from "./js/refs";
-import { handlerReg } from "./js/reg";
+import { handlerLogin } from './js/login';
+import { regBtn, logBtn, STORAGE_KEY } from './js/refs';
+import { handlerReg } from './js/reg';
 
 regBtn.addEventListener('click', handlerReg);
+
+logBtn.addEventListener('click', handlerLogin);
+
+const token = localStorage.getItem(STORAGE_KEY);
+if (token !== null) {
+  location.replace('/contacts.html');
+}
