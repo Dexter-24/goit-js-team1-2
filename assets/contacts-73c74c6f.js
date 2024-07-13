1e483cd9dd3489e12014b623eae4f4764bd4d247
Dexter-24 <67460733+Dexter-24@users.noreply.github.com>
2024-07-13 09:52:44 +0000

    Deploying to gh-pages from @ Dexter-24/goit-js-team1-2@8221927519950dafbf122f636f49ac8717d844ff 🚀

--- assets/contacts-73c74c6f.js
+++
@@ -1,7 +0,0 @@
-import{S as r,c as a,a as o,b as s}from"./api-1b40d5da.js";const l=({id:e,name:t,number:n})=>`
-      <li class="list-item" id="${e}">
-      <p>Name: ${t}</p>
-      <p>Number: ${n}</p>
-      <button class="delete-btn">Delete</button>
-      </li>
-     `,i=localStorage.getItem(r);i===null&&location.replace("/");a.addEventListener("submit",m);function m(e){e.preventDefault();const{name:t,number:n}=e.currentTarget.elements;o({name:t.value,number:n.value}).then(c=>{s.insertAdjacentHTML("beforeend",l(c)),a.reset()})}
