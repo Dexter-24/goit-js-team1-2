40c31f1e65fbb94aac2dc3192b88f99b6d27d228
Serhii Bova <serschgold@gmail.com>
2024-07-13 12:51:59 +0300

    logout

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,5 +3,8 @@ const logBtn = document.querySelector('.login-btn');
 const contactForm = document.querySelector('.contact-form');
 const contactList = document.querySelector('.contacts-list');
 const STORAGE_KEY = 'token';
+const btnLogOut = document.querySelector(".log-out-btn")
 
-export { regBtn, logBtn, STORAGE_KEY, contactForm, contactList };
+export { regBtn, logBtn, STORAGE_KEY, contactForm, contactList, btnLogOut };
+
+
